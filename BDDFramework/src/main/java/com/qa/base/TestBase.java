6d8c55e78b605e29cc0d64e83381b51e2aced1b6
swadhin <swadhin@swadhin-pc>
2019-04-07 17:25:54 +0530

    commit

--- BDDFramework/src/main/java/com/qa/base/TestBase.java
+++ BDDFramework/src/main/java/com/qa/base/TestBase.java
@@ -142,7 +142,7 @@ public class TestBase {
 			  driver.manage().deleteAllCookies();
 			  driver.manage().timeouts().pageLoadTimeout(80, TimeUnit.SECONDS);
 			  driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
-			  driver.get("https://www.google.com1/");
+			  driver.get("https://www.google.com/");
 			  
 	       
 	    } 
